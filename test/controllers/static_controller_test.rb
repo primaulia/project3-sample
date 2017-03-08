242a38b595bcd8392dacf8595166a0464c23b092
Prima Aulia Gusta <primaulia@gmail.com>
2017-03-08 13:40:56 +0800

    update travis

--- test/controllers/static_controller_test.rb
+++ test/controllers/static_controller_test.rb
@@ -2,8 +2,7 @@ require 'test_helper'
 
 class StaticControllerTest < ActionDispatch::IntegrationTest
   test "should get homepage" do
-    get static_homepage_url
+    get root_url
     assert_response :success
   end
-
 end
